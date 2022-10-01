cae9a5dd2c37d6f2e0104da8d5a02cbcd11e5cc2
Varga József Márk <vmark2145@gmail.com>
2022-10-01 11:55:36 +0300

    Initial import

---
+++ src/test/java/com/markvarga21/filmadministrator/FilmAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markvarga21.filmadministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
