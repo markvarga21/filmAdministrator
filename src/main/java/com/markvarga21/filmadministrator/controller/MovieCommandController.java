ed13ed90c0121a9a2e2d3f7c59aff97a9bfc0bd3
Varga József Márk <vmark2145@gmail.com>
2022-10-01 15:45:28 +0300

    Refactored movie classes

--- src/main/java/com/markvarga21/filmadministrator/controller/MovieCommandController.java
+++ src/main/java/com/markvarga21/filmadministrator/controller/MovieCommandController.java
@@ -3,6 +3,7 @@ package com.markvarga21.filmadministrator.controller;
 import com.markvarga21.filmadministrator.dto.MovieDTO;
 import com.markvarga21.filmadministrator.service.MovieService;
 import com.markvarga21.filmadministrator.service.SigningService;
+import com.markvarga21.filmadministrator.util.Formatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
@@ -11,18 +12,10 @@ import org.springframework.shell.standard.ShellMethodAvailability;
 
 @ShellComponent
 @RequiredArgsConstructor
-public class CreateCommandController {
+public class MovieCommandController {
     private final SigningService signingService;
     private final MovieService movieService;
-
-    @ShellMethod(value = "Creating movies", key = "create movie")
-    @ShellMethodAvailability("checkAdmin")
-    public String createMovie(String name, String genre, Long length) {
-        MovieDTO movieToSave = new MovieDTO(name, genre, length);
-        this.movieService.saveMovie(movieToSave);
-        return String.format("Movie '%s' with the length of %d created!", name, length);
-    }
-
+    private final Formatter formatter;
     public Availability checkAdmin() {
         boolean isAdminLoggedIn = this.signingService.isAdminLoggedIn();
         return isAdminLoggedIn ?  Availability.available() : Availability.unavailable("Only admin has access for this command!");
@@ -32,4 +25,25 @@ public class CreateCommandController {
         boolean isSomeoneLoggedIn = this.signingService.isSomeoneLoggedIn();
         return isSomeoneLoggedIn ?  Availability.available() : Availability.unavailable("Please login before executing this command!");
     }
+
+    public Availability anyone() {
+        return Availability.available();
+    }
+
+    @ShellMethod(value = "Creating movies", key = "create movie")
+    @ShellMethodAvailability("checkAdmin")
+    public String createMovie(String name, String genre, Long length) {
+        MovieDTO movieToSave = new MovieDTO(name, genre, length);
+        this.movieService.saveMovie(movieToSave);
+        return String.format("Movie '%s' with the length of %d created!", name, length);
+    }
+
+    @ShellMethod(value = "Listing movies.", key = "list movies")
+    @ShellMethodAvailability("anyone")
+    public String listMovies() {
+        var movies = this.movieService.getMovies();
+        return this.formatter.formatMovieList(movies);
+    }
+
+
 }
